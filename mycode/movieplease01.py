f0de62d01f1eafb2608b2c62e8b3508eddf0902c
Rasesh Maniar <maniara@txsldlnx1b02.local>
2018-11-05 14:15:04 -0600

    your commit message

---
+++ mycode/movieplease01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir('/Users/maniara/mycode/')
+shutil.move('raynor.obj', 'ceph_storage/')
+
+# The following line will rename a file
+xname = input('What is the new name for kerrigan.obj? ')
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
